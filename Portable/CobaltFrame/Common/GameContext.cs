d6be2f31c5441263881cc3f0a3bf3c6f73be6811
garicchi <xgaricchi@gmail.com>
2015-03-26 06:22:48 +0900

    Add GameContext

---
+++ Portable/CobaltFrame/Common/GameContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobaltFrame.Common
+{
+    public class GameContext
+    {
+        private Game _game;
+
+        public GameContext(Game game)
+        {
+            this._game = game;
+        }
+    }
+}
---
+++ Portable/CobaltGame.Android/MainGame.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CobaltFrame;
+
+namespace CobaltGame.Android
+{
+    class MainGame:Game
+    {
+        GraphicsDeviceManager graphics;
+        SampleComponent component;
+        public MainGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            component = new SampleComponent(this);
+            Components.Add(component);
+        }
+
+        protected override void Initialize()
+        {
+            
+            base.Initialize();
+        }
+    }
+}
