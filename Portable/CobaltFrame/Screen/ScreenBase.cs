0eadbd4285025595e8e4b326a0870a15714c6f1b
garicchi <xgaricchi@gmail.com>
2015-03-26 07:26:05 +0900

    ScreenBase add

---
+++ Portable/CobaltFrame/Screen/ScreenBase.cs
@@ -0,0 +1,44 @@
+﻿using CobaltFrame.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobaltFrame.Screen
+{
+    public abstract class ScreenBase:IScreen
+    {
+        protected ScreenContext _screenContext;
+
+        public ScreenBase(ScreenContext screenContext)
+        {
+            this._screenContext = screenContext;
+        }
+
+        public void Initialize(ScreenContext screenContext)
+        {
+           
+        }
+
+        public void LoadScreen()
+        {
+            
+        }
+
+        public void UnloadScreen()
+        {
+            
+        }
+
+        public IScreen Update(ScreenFrameContext frameContext)
+        {
+            return this;
+        }
+
+        public void Draw(ScreenFrameContext frameContext)
+        {
+           
+        }
+    }
+}
