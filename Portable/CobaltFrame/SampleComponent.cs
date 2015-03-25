9a180b4f152e3cadc6ca1d44757b2e635e0688ef
garicchi <xgaricchi@gmail.com>
2015-03-26 03:50:11 +0900

    Add Android Project

---
+++ Portable/CobaltFrame/SampleComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobaltFrame
+{
+    public class SampleComponent:GameComponent
+    {
+        public SampleComponent(Game game)
+            :base(game)
+        {
+
+        }
+        public override void Initialize()
+        {
+            for (int i = 0; i < 20;i++ )
+                Debug.WriteLine("hoge");
+            base.Initialize();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            
+            base.Update(gameTime);
+        }
+    }
+}
