4781d489288c2497f82a7be657c4edf3cc791553
garicchi <xgaricchi@gmail.com>
2015-03-26 07:04:00 +0900

    GameObject追加

---
+++ Portable/CobaltFrame/Object/GameObject.cs
@@ -0,0 +1,42 @@
+﻿using CobaltFrame.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobaltFrame.Object
+{
+    public abstract class GameObject:IGameObject
+    {
+        protected ObjectContext _objectContext;
+        public GameObject(ObjectContext objectContext)
+        {
+            this._objectContext = objectContext;
+        }
+        public void Initialize()
+        {
+            
+        }
+
+        public void LoadContent()
+        {
+            
+        }
+
+        public void UnloadContent()
+        {
+            
+        }
+
+        public void Update(Common.ObjectFrameContext frameContext)
+        {
+            
+        }
+
+        public void Draw(Common.ObjectFrameContext frameContext)
+        {
+            
+        }
+    }
+}
