d968e4ea160af5c9ff796538e2f4f183974b3ef2
garicchi <xgaricchi@gmail.com>
2015-03-26 06:32:51 +0900

    FrameContextBase追加

---
+++ Portable/CobaltFrame/Common/FrameContextBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobaltFrame.Common
+{
+    public abstract class FrameContextBase
+    {
+        Game _game;
+
+        public Game Game { get { return this._game; } }
+        public FrameContextBase(Game game)
+        {
+            this._game = game;
+        }
+    }
+}
