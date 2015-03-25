58bcf2b2413af9191541e0340ef5519fd7363354
garicchi <xgaricchi@gmail.com>
2015-03-26 06:30:26 +0900

    ObjectContext追加

---
+++ Portable/CobaltFrame/Common/ObjectContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobaltFrame.Common
+{
+    public class ObjectContext:GameContextBase
+    {
+        public ObjectContext(Game game)
+            :base(game)
+        {
+
+        }
+    }
+}
