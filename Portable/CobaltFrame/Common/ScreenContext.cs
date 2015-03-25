d22ac463e72aed12dd9ea251cc056b1dacbd9b77
garicchi <xgaricchi@gmail.com>
2015-03-26 06:28:52 +0900

    ScreenContext追加

---
+++ Portable/CobaltFrame/Common/ScreenContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobaltFrame.Common
+{
+    public class ScreenContext:GameContextBase
+    {
+        public ScreenContext(Game game)
+            : base(game)
+        {
+
+        }
+    }
+}
