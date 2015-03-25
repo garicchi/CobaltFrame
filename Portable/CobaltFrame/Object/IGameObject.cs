e2c533c1f3a42188a47124e74d7132369f3032bd
garicchi <xgaricchi@gmail.com>
2015-03-26 06:58:50 +0900

    IGameObjectの作成

---
+++ Portable/CobaltFrame/Object/IGameObject.cs
@@ -0,0 +1,19 @@
+﻿using CobaltFrame.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobaltFrame.Object
+{
+    public interface IGameObject
+    {
+        public void Initialize();
+        public void LoadContent();
+        public void UnloadContent();
+        public void Update(ObjectFrameContext frameContext);
+        public void Draw(ObjectFrameContext frameContext);
+        
+    }
+}
