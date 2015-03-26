dfc33636fd1f63f690433565f0ea59f3538d3841
garicchi <xgaricchi@gmail.com>
2015-03-26 17:17:26 +0900

    AnimationBase追加その2

---
+++ Portable/CobaltFrame/Animation/IAnimation.cs
@@ -0,0 +1,18 @@
+﻿using CobaltFrame.Object;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobaltFrame.Animation
+{
+    public interface IAnimation:IGameObject
+    {
+        void Start(TimeSpan duration,bool isLoop);
+        void Pause();
+
+        void Resume();
+        void Stop();
+    }
+}
