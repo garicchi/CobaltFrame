b8cef7567fec6a83e467ec0e35800646be452009
garicchi <xgaricchi@gmail.com>
2015-03-26 17:17:12 +0900

    AnimationBase作成

---
+++ Portable/CobaltFrame/Animation/AnimationState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobaltFrame.Animation
+{
+    public enum AnimationState
+    {
+        Active,Pause,Stop
+    }
+}
