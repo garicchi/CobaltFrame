3b0cdcc9c75ed03509bbe1e7322c69d71b59d888
garicchi <xgaricchi@gmail.com>
2015-04-19 02:42:47 +0900

    IBox2修正

--- Portable/CobaltFrame.Mono/Position/IBox2.cs
+++ Portable/CobaltFrame.Mono/Position/IBox2.cs
@@ -9,17 +9,17 @@ namespace CobaltFrame.Mono.Position
 {
     public interface IBox2
     {
-        public void SetRect(Rectangle rect);
-        public Rectangle GetRect();
-        public void SetLocation(Vector2 vec);
-        public Vector2 GetLocation();
-        public Vector2 GetCenter();
-        public void SetCenter(Vector2 vec);
-        public void MoveRect(int up=0,int left=0,int down=0,int right=0);
-        public IBox2 TryMoveRect(int up = 0, int left = 0, int down = 0, int right = 0);
-        public bool Contains(int x,int y);
-        public bool Contains(Vector2 vec);
-        public bool Contains(IBox2 box);
-        public bool Intersects(IBox2 box);
+        void SetRect(Rectangle rect);
+        Rectangle GetRect();
+        void SetLocation(Vector2 vec);
+        Vector2 GetLocation();
+        Vector2 GetCenter();
+        void SetCenter(Vector2 vec);
+        void MoveRect(int up=0,int left=0,int down=0,int right=0);
+        IBox2 TryMoveRect(int up = 0, int left = 0, int down = 0, int right = 0);
+        bool Contains(int x,int y);
+        bool Contains(Vector2 vec);
+        bool Contains(IBox2 box);
+        bool Intersects(IBox2 box);
     }
 }
