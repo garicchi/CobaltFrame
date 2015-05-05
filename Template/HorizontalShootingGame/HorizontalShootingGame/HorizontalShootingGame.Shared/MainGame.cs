f94214dccaea219a6853bb2a5766dd4ee5d9baa8
garicchi <xgaricchi@gmail.com>
2015-05-05 19:51:47 +0900

    リファクタリング

--- Template/HorizontalShootingGame/HorizontalShootingGame/HorizontalShootingGame.Shared/MainGame.cs
+++ Template/HorizontalShootingGame/HorizontalShootingGame/HorizontalShootingGame.Shared/MainGame.cs
@@ -19,114 +19,126 @@ using Microsoft.Xna.Framework.Storage;
 
 namespace HorizontalShootingGame
 {
-	public class MainGame:Game
-	{
-		GameManager _gameManager;
+    public class MainGame : Game
+    {
+        GameManager _gameManager;
 
-		public MainGame ()
-		{
-			Content.RootDirectory = "Content";
+        public MainGame()
+        {
+            Content.RootDirectory = "Content";
 
-			this._gameManager = new GameManager (this, new LoadScreen (), new Vector2 (1360, 768), ScaleMode.Fill);
-			GameContext.GraphicsManager.IsFullScreen = true;
+            this._gameManager = new GameManager(this, new LoadScreen(), new Vector2(1360, 768), ScaleMode.Fill);
+            GameContext.GraphicsManager.IsFullScreen = true;
 
-			GameContext.Game.Activated += (s, e) => {
-				SaveDataStore<SaveData>.Load (new SaveData ());
-			};
-			GameContext.Game.Deactivated += (s, e) => {
-				SaveDataStore<SaveData>.Save ();
-			};
+            GameContext.Game.Activated += (s, e) =>
+            {
+                SaveDataStore<SaveData>.Load(new SaveData());
+            };
+            GameContext.Game.Deactivated += (s, e) =>
+            {
+                SaveDataStore<SaveData>.Save();
+            };
 
-			SaveDataStore<SaveData>.Setup ("__savedata", (name) => {
-				SaveData data = null;
-				var deviceResult = StorageDevice.BeginShowSelector (null, null);
-				deviceResult.AsyncWaitHandle.WaitOne ();
-				var device = StorageDevice.EndShowSelector (deviceResult);
-				var containerResult = device.BeginOpenContainer (name, null, null);
-				containerResult.AsyncWaitHandle.WaitOne ();
-				using (var container = device.EndOpenContainer (containerResult)) {
+            SaveDataStore<SaveData>.Setup("__savedata", (name) =>
+            {
+                SaveData data = null;
+                var deviceResult = StorageDevice.BeginShowSelector(null, null);
+                deviceResult.AsyncWaitHandle.WaitOne();
+                var device = StorageDevice.EndShowSelector(deviceResult);
+                var containerResult = device.BeginOpenContainer(name, null, null);
+                containerResult.AsyncWaitHandle.WaitOne();
+                using (var container = device.EndOpenContainer(containerResult))
+                {
 
-					if (container.FileExists (name)) {
-						using (var stream = container.OpenFile (name, FileMode.Open)) {
-							var serializer = new XmlSerializer (typeof(SaveData));
-							data = (SaveData)serializer.Deserialize (stream);
-						}
+                    if (container.FileExists(name))
+                    {
+                        using (var stream = container.OpenFile(name, FileMode.Open))
+                        {
+                            var serializer = new XmlSerializer(typeof(SaveData));
+                            data = (SaveData)serializer.Deserialize(stream);
+                        }
 
-					}
-				}
-				return data;
+                    }
+                }
+                return data;
 
-			}, (name, data) => {
-				try {
-					var deviceResult = StorageDevice.BeginShowSelector (null, null);
-					deviceResult.AsyncWaitHandle.WaitOne ();
-					var device = StorageDevice.EndShowSelector (deviceResult);
-					var containerResult = device.BeginOpenContainer (name, null, null);
-					containerResult.AsyncWaitHandle.WaitOne ();
-					using (var container = device.EndOpenContainer (containerResult)) {
-						if (container.FileExists (name))
-							container.DeleteFile (name);
+            }, (name, data) =>
+            {
+                try
+                {
+                    var deviceResult = StorageDevice.BeginShowSelector(null, null);
+                    deviceResult.AsyncWaitHandle.WaitOne();
+                    var device = StorageDevice.EndShowSelector(deviceResult);
+                    var containerResult = device.BeginOpenContainer(name, null, null);
+                    containerResult.AsyncWaitHandle.WaitOne();
+                    using (var container = device.EndOpenContainer(containerResult))
+                    {
+                        if (container.FileExists(name))
+                            container.DeleteFile(name);
 
-						using (var stream = container.CreateFile (name)) {
-							var serializer = new XmlSerializer (typeof(SaveData));
-							serializer.Serialize (stream, data);
+                        using (var stream = container.CreateFile(name))
+                        {
+                            var serializer = new XmlSerializer(typeof(SaveData));
+                            serializer.Serialize(stream, data);
 
-						}
-					}
+                        }
+                    }
 
-					return true;
-				} catch (Exception) {
-					return false;
-				}
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
 
 
-			});
+            });
 
-			/*
+            /*
             GameInput.SetupAccelState(() =>
             {
 
                 return new AccelerometerState(new Vector3(1,0,0));
             });
             */
-		}
+        }
 
 
 
-		protected override void Initialize ()
-		{
-			this._gameManager.Init ();
-			base.Initialize ();
+        protected override void Initialize()
+        {
+            this._gameManager.Init();
+            base.Initialize();
 
 
-		}
+        }
 
-		protected override void LoadContent ()
-		{
-			this._gameManager.Load ();
-			base.LoadContent ();
+        protected override void LoadContent()
+        {
+            this._gameManager.Load();
+            base.LoadContent();
 
-		}
+        }
 
-		protected override void UnloadContent ()
-		{
-			this._gameManager.Unload ();
-			base.UnloadContent ();
+        protected override void UnloadContent()
+        {
+            this._gameManager.Unload();
+            base.UnloadContent();
 
-		}
+        }
 
-		protected override void Update (GameTime gameTime)
-		{
-			base.Update (gameTime);
-			this._gameManager.Update (new FrameContext (gameTime));
-		}
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            this._gameManager.Update(new FrameContext(gameTime));
+        }
 
-		protected override void Draw (GameTime gameTime)
-		{
-			base.Draw (gameTime);
-			this._gameManager.Draw (new FrameContext (gameTime));
-		}
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            this._gameManager.Draw(new FrameContext(gameTime));
+        }
 
 
-	}
+    }
 }
